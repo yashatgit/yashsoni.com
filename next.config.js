936446a08218e3b622954a7cb44823e9a1a85a7d
yashSoni <ysoni@sprinklr.com>
2020-06-22 11:25:05 +0530

    re-enable pwa

--- next.config.js
+++ next.config.js
@@ -20,9 +20,9 @@ module.exports = withPWA(
 
       return config;
     },
-    // pwa: {
-    //   disable: process.env.NODE_ENV === 'development',
-    //   dest: 'public',
-    // },
+    pwa: {
+      disable: process.env.NODE_ENV === 'development',
+      dest: 'public',
+    },
   })
 );
