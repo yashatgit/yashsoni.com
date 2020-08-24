8254548c81a88a6ed6626e95c539b2fb4db792af
yashSoni <ysoni@sprinklr.com>
2020-08-24 11:53:52 +0530

    language tabs in code

--- components/elements/code.js
+++ components/elements/code.js
@@ -1,13 +1,41 @@
 import React from 'react';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import styled from 'styled-components';
 
-const Code = ({ children, language }) => {
+const StyledCodeBlock = styled.div`
+  position: relative;
+
+  ${props =>
+    props.language &&
+    `
+    &:before {
+      content: '${props.language || ''}';
+      position: absolute;
+      top: 0px;
+      right: 1rem;
+      font-size: 0.7rem;
+      letter-spacing: 0.8px;
+      text-transform: uppercase;
+      color: rgb(34, 34, 47);
+      background: rgb(247, 223, 30);
+      padding: 2px 10px;
+      border-radius: 0px 0px 5px 5px;      
+    }
+    `}
+`;
+
+const Code = ({ children }) => {
   const codeElementProps = children.props;
 
+  //https://mdxjs.com/guides/syntax-highlighting
+  const language = codeElementProps.className?.replace(/language-/, '') ?? '';
+
   return (
-    <SyntaxHighlighter language={language} style={{ overflow: 'scroll' }} useInlineStyles={false}>
-      <>{codeElementProps.children}</>
-    </SyntaxHighlighter>
+    <StyledCodeBlock language={language}>
+      <SyntaxHighlighter language={language} style={{ overflow: 'scroll' }} useInlineStyles={false}>
+        <>{codeElementProps.children}</>
+      </SyntaxHighlighter>
+    </StyledCodeBlock>
   );
 };
 
