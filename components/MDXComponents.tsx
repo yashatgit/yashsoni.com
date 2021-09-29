1fadc21e8aa3c0e8998e6808337fccd2d655575c
yashSoni <ysoni@sprinklr.com>
2021-09-29 19:17:43 +0400

    adds code tag

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -5,6 +5,12 @@ import Book from 'components/Book';
 import { NN_Lazy } from 'data/blog/neural-networks-hello-world/NN_Lazy';
 import { CNN_Handwriting_MNIST_Lazy } from 'data/blog/handwriting-recognition-using-CNN/CNN_Handwriting_MNIST_Lazy';
 
+function getLanguage(str) {
+  const regexp = /language-([^\s]+)/g;
+  const matches = [...str.matchAll(regexp)];
+  return matches[0]?.[1];
+}
+
 import MediaGrid from 'components/mediaGrid';
 
 const CustomLink = (props) => {
@@ -22,6 +28,16 @@ const CustomLink = (props) => {
   return <a target="_blank" rel="noopener noreferrer" {...props} />;
 };
 
+const CustomPre = (props) => {
+  const language = getLanguage(props.children.props?.className ?? '');
+  return (
+    <div className="relative">
+      {language ? <span className="code-tag">{language}</span> : null}
+      <pre>{props.children}</pre>
+    </div>
+  );
+};
+
 const MDXComponents = {
   Image,
   a: CustomLink,
@@ -29,6 +45,7 @@ const MDXComponents = {
   Book,
   MediaGrid,
   NN_Lazy,
+  pre: CustomPre,
   CNN_Handwriting_MNIST_Lazy
 };
 
