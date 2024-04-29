3f973ee98bde6866612e441d45455eee23265bb2
Yash Soni <mailme@yashsoni.com>
2024-04-29 14:46:36 +0400

    setup OG image

--- TODO.md
+++ TODO.md
@@ -1,7 +1,2 @@
 book component
-
 TF graph plot issue
-
-setup OG
-
-favicon
--- app/og/route.tsx
+++ app/og/route.tsx
@@ -21,7 +21,7 @@ export async function GET(req: NextRequest) {
           flexDirection: "column",
           alignItems: "flex-start",
           justifyContent: "center",
-          backgroundImage: "url(https://yashsoni.com/og-bg.png)",
+          backgroundImage: "url(https://yashsoni.com/og-bg-author.jpeg)",
         }}
       >
         <div
