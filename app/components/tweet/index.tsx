39220edfb2107bc685a1b98a8add7f578282c5db
Yash Soni <mailme@yashsoni.com>
2024-05-23 18:13:10 +0400

    Deno Jupyter

--- app/components/tweet/index.tsx
+++ app/components/tweet/index.tsx
@@ -1,12 +1,7 @@
-import { getTweet } from 'react-tweet/api';
-import { Suspense } from 'react';
-import {
-  TweetSkeleton,
-  EmbeddedTweet,
-  TweetNotFound,
-  type TweetProps,
-} from 'react-tweet';
-import './tweet.css';
+import { getTweet } from "react-tweet/api";
+import { Suspense } from "react";
+import { TweetSkeleton, EmbeddedTweet, TweetNotFound, type TweetProps } from "react-tweet";
+import "./tweet.css";
 
 const TweetContent = async ({ id, components, onError }: TweetProps) => {
   let error;
